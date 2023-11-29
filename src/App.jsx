647232c05087537357bd93594e97816fc64774b1
heroesluk <242209@edu.p.lodz.pl>
2023-11-29 15:53:18 +0000

    merge from imageGrid

--- src/App.jsx
+++ src/App.jsx
@@ -38,50 +38,6 @@ export default function App() {
     </BrowserRouter>);
 }
 
-function ImageGrid() {
-    const data = {
-        "data": [
-            {
-                "folder_id": 1,
-                "id": 1,
-                "image_add_date": "Sun, 01 Jan 2023 12:00:00 GMT",
-                "image_size": 1024,
-                "name": "image1.jpg"
-            },
-            {
-                "folder_id": 1,
-                "id": 2,
-                "image_add_date": "Mon, 02 Jan 2023 14:30:00 GMT",
-                "image_size": 2048,
-                "name": "image2.jpg"
-            },
-            {
-                "folder_id": 1,
-                "id": 3,
-                "image_add_date": "Tue, 03 Jan 2023 10:45:00 GMT",
-                "image_size": 1536,
-                "name": "image3.jpg"
-            }
-        ]
-    }
-
-    return (<ImageList sx={{width: 500, height: 450}} cols={3} rowHeight={164}>
-        {itemData.map((item) => (
-            <ImageListItem key={item.img}>
-                <img
-                    src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-                    srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                    alt={item.title}
-                    loading="lazy"
-                />
-            </ImageListItem>
-        ))}
-    </ImageList>)
-}
-
-
-
-
 
 
 
